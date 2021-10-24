e691e9b62ecb74c217f7f755359418a3b99d7ea6
browntulstar <92661580+browntulstar@users.noreply.github.com>
2021-10-24 14:14:45 -0700

    Added Killjoy to the level

---
+++ tilesets/KJTileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="KJTileset" tilewidth="30" tileheight="40" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="30" height="40" source="../images/kjchibi.png"/>
+ </tile>
+</tileset>
