717d2441716897850375010639e9def6edc84aaa
browntulstar <92661580+browntulstar@users.noreply.github.com>
2021-10-24 14:43:55 -0700

    Added Killjoy Alarm Bot Objective

--- levels/vr_mission_template/objectives/example_objective/description.md
+++ levels/vr_mission_template/objectives/example_objective/description.md
@@ -1,21 +1,20 @@
 # Example Objective
 
 <div class="aside">
-<h3>To-Do List</h3>
+<h3>Killjoy's To-do List</h3>
 <ul>
-  <li>It's good to keep high level tasks here.</li>
-  <li>Let players know when to click <em>HACK</em>.</li>
+  <li>Analyze the performance of working alarm bots.</li>
+  <li>Observe how long it takes for Killjoy's alarm bot to cool down, and how much damage it does.</li>
+  <li>After logging your analysis on the right, click <em>HACK</em>.</li>
 </ul>
 </div>
 
-Give players a little bit of information about what they need to do to hack past this objective. If you find yourself writing more than a few short paragraphs of text, it's likely your objective is too big!
+Killjoy is doing some tinkering on her alarm bots but lost the documentation to her 3.07 patched version of them. She needs your help to find the numbers to her newest alarm bots so that they can be balanced and used against the enemy team.
 
-## You can make sub headers with more information
+![image of killjoy](https://firstsportz.com/wp-content/uploads/2021/05/IMG-20210513-WA0333-696x392.jpg)
 
-Some times you wan to talk about additional concepts beyond the high level goal that might help players accomplish the high level goal.
+In order to do this, you will need to go into a **Custom Match** with a friend of yours and test the behavior of the alarm bot, including its proximity sensors and damage output. 
 
-```js
-const areCodeSamplesVeryHelpful = true;
-```
+## Task
 
-Describe the final values for your player one last time. Then ask them to click the _HACK_ button.
+Log your findings in your report that the rest of the team will read. Well, did anyone read the report? I hope they do. Afterwards, click the _HACK_ button.
--- levels/vr_mission_template/objectives/example_objective/validator.js
+++ levels/vr_mission_template/objectives/example_objective/validator.js
@@ -18,31 +18,38 @@ have completed the challenge as instructed.
 */
 module.exports = async function (helper) {
   // We start by getting the user input from the helper
-  const { answer1, answer2 } = helper.validationFields;
+  const { answer1, answer2, answer3 } = helper.validationFields;
 
   // Next, you test the user input - fail fast if they get one of the
   // answers wrong, or some aspect is wrong! Don't provide too much
   // negative feedback at once, have the player iterate.
-  if (!answer1 || !isTwilio(answer1)) {
+  if (!answer1 || Number(answer1) !== 20) {
     return helper.fail(`
-      The answer to the first question is incorrect. The company that
-      makes TwilioQuest starts with a "T" and ends with a "wilio".
+      The first answer is incorrect. Think about how many seconds it takes
+      to get that alarm bot cooled down! (Hint: it's more than 10 seconds 
+      and less than 30 seconds)
     `);
   }
 
   // You can use npm or core Node.js dependencies in your validators!
   try {
-    assert.strictEqual(R.add(2, 2), Number(answer2));
+    assert.strictEqual(0, Number(answer2));
   } catch (e) {
     return helper.fail(`
-      The second answer you provided was either not a number, or not the
-      correct response for "what is 2 + 2".
+      The second answer is incorrect. Think about the effects of the alarm
+      bot and what its primary use case is.
+    `);
+  }
+
+  if (!answer3 || answer3 === "true") {
+    return helper.fail(`
+      The third answer is incorrect. Can you really recall something that is destroyed?
     `);
   }
 
   // The way we usually write validators is to fail fast, and then if we reach
   // the end, we know the user got all the answers right!
   helper.success(`
-    Hooray! You did it!
+    Hooray! You did it! You can trust Killjoy's bots now!
   `);
 };
